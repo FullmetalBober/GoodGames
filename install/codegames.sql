f70f3c89cf9536ce151b33f3b7521da3ea703cef
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2022-12-29 14:51:19 +0200

    update category phpMyAdmin

--- install/codegames.sql
+++ install/codegames.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Час створення: Гру 28 2022 р., 17:27
+-- Час створення: Гру 29 2022 р., 15:49
 -- Версія сервера: 8.0.30
 -- Версія PHP: 8.1.9
 
@@ -29,39 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `category` (
   `id` int NOT NULL COMMENT 'ID',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Назва категорії'
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Назва категорії',
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп даних таблиці `category`
 --
 
-INSERT INTO `category` (`id`, `name`) VALUES
-(1, 'Ноутбуки'),
-(2, 'Планшети'),
-(3, 'Телефони'),
-(4, 'Генеретори'),
-(5, 'Павербанки');
-
--- --------------------------------------------------------
-
---
--- Структура таблиці `producer`
---
-
-CREATE TABLE `producer` (
-  `id` int NOT NULL COMMENT 'ID',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Назва виробника'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Дамп даних таблиці `producer`
---
-
-INSERT INTO `producer` (`id`, `name`) VALUES
-(1, 'LENOVO'),
-(2, 'Asus'),
-(3, 'Acer');
+INSERT INTO `category` (`id`, `name`, `photo`) VALUES
+(1, 'Ноутбуки', ''),
+(2, 'Планшети', ''),
+(3, 'Телефони', ''),
+(4, 'Генеретори', ''),
+(5, 'Павербанки', ''),
+(6, 'test', ''),
+(7, 'qwe', '123456789.jpg');
 
 -- --------------------------------------------------------
 
@@ -74,18 +57,20 @@ CREATE TABLE `product` (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Назва товару',
   `producer_id` int NOT NULL COMMENT 'Виробник',
   `category_id` int NOT NULL COMMENT 'Категорія',
-  `price` float NOT NULL COMMENT 'Ціна',
+  `price` double NOT NULL COMMENT 'Ціна',
   `count` int NOT NULL COMMENT 'Кількість',
-  `short_text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT 'Короткий опис товару'
+  `short_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT 'Короткий опис товару',
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `visible` int NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп даних таблиці `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `producer_id`, `category_id`, `price`, `count`, `short_text`) VALUES
-(1, 'Ноутбук Lenovo Vostro 15 3500', 1, 1, 25000, 12, 'Тут буде короткий опис ноутбуку'),
-(2, 'Ноутбук Lenovo Vostro 15 3600', 1, 1, 27500, 11, 'Тут буде короткий опис ноутбуку');
+INSERT INTO `product` (`id`, `name`, `producer_id`, `category_id`, `price`, `count`, `short_description`, `description`, `visible`) VALUES
+(1, 'Ноутбук Lenovo Vostro 15 3500', 1, 1, 25000, 12, 'Тут буде короткий опис ноутбуку', '', 1),
+(2, 'Ноутбук Lenovo Vostro 15 3600', 1, 1, 27500, 11, 'Тут буде короткий опис ноутбуку', '', 1);
 
 -- --------------------------------------------------------
 
@@ -98,23 +83,25 @@ CREATE TABLE `user` (
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL
+  `firstname` varchar(255) NOT NULL,
+  `access_level` int NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп даних таблиці `user`
 --
 
-INSERT INTO `user` (`id`, `login`, `password`, `lastname`, `firstname`) VALUES
-(1, 'mankivskiy.vlsd@gmail.com', 'admin', 'Mankivskyi', 'Vladyslav'),
-(8, 'mankivskyi.vlad@gmail.com', 'test', 'test', ''),
-(9, 'mankivskyi@gmail.com', 'test', 'test', 'test'),
-(10, 'mankivskyi3@gmail.com', 'test', 'test', 'test'),
-(11, 'mankivskyi4@gmail.com', 'test', 'test', 'test'),
-(12, 'mankivskyi5@gmail.com', 'test', 'test', 'test'),
-(13, 'mankivskyi6@gmail.com', 'test', 'test', 'test'),
-(14, 'mankivskyi7@gmail.com', 'test', 'test', 'test'),
-(15, 'mankivskyi8@gmail.com', 'test', 'test', 'test');
+INSERT INTO `user` (`id`, `login`, `password`, `lastname`, `firstname`, `access_level`) VALUES
+(1, 'mankivskiy.vlsd@gmail.com', 'admin', 'Mankivskyi', 'Vladyslav', 10),
+(8, 'mankivskyi.vlad@gmail.com', 'test', 'test', '', 1),
+(9, 'mankivskyi@gmail.com', 'test', 'test', 'test', 1),
+(10, 'mankivskyi3@gmail.com', 'test', 'test', 'test', 1),
+(11, 'mankivskyi4@gmail.com', 'test', 'test', 'test', 1),
+(12, 'mankivskyi5@gmail.com', 'test', 'test', 'test', 1),
+(13, 'mankivskyi6@gmail.com', 'test', 'test', 'test', 1),
+(14, 'mankivskyi7@gmail.com', 'test', 'test', 'test', 1),
+(15, 'mankivskyi8@gmail.com', 'test', 'test', 'test', 1),
+(16, 'test@gmail.com', 'test', 'test', 'test', 1);
 
 --
 -- Індекси збережених таблиць
@@ -124,12 +111,6 @@ INSERT INTO `user` (`id`, `login`, `password`, `lastname`, `firstname`) VALUES
 -- Індекси таблиці `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Індекси таблиці `producer`
---
-ALTER TABLE `producer`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -154,13 +135,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT для таблиці `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT для таблиці `producer`
---
-ALTER TABLE `producer`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID', AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблиці `product`
@@ -172,7 +147,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT для таблиці `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Обмеження зовнішнього ключа збережених таблиць
@@ -182,8 +157,7 @@ ALTER TABLE `user`
 -- Обмеження зовнішнього ключа таблиці `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
-  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`producer_id`) REFERENCES `producer` (`id`);
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
