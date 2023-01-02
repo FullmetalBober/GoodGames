5b40d0cd89af7ea487c9ba1803ccc981431e4db4
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2023-01-02 17:03:09 +0200

    project name => GoodGames

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
 define('DATABASE_HOSTNAME', 'localhost');
-define('DATABASE_LOGIN', 'codegames');
+define('DATABASE_LOGIN', 'goodgames');
 define('DATABASE_PASSWORD', '7p.bDzy6/xylcM[P');
-define('DATABASE_NAME', 'codegames');
+define('DATABASE_NAME', 'goodgames');
